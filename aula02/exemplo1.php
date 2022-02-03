211a5e50ef817866ec96657593cbafd0e8c6d3f9
Jonathan Levy <jonathan.adm2012@gmail.com>
2022-02-03 20:21:50 -0300

    exemplo de codigo (// e #)para comentarios  :cat:

---
+++ aula02/exemplo1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <?php
+    // Este é um comentário de uma linha
+
+    # Este também é um comentário de uma linha
+     
+    // echo "Olá mundo";
+    ?>
+
+</body>
+
+</html>

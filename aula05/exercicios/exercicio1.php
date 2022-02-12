6bb69b2bb779c07935bb589b6b6ebff384508dc5
Jonathan Levy <jonathan.adm2012@gmail.com>
2022-02-11 21:31:22 -0300

    exercitando tipos de dados

---
+++ aula05/exercicios/exercicio1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EXERCICIO1</title>
+</head>
+<body>
+    <?php
+        $segunda_feira = "Segunda-feira"; 
+        $terca_feira = "Terça-feira";
+        $quarta_feira = "Quarta-feira";
+        $quinta_feira = "Quinta-feira";
+        $sexta_feira = "Sexta-feira";
+        $sabado = "Sabado";
+        $domingo = "Domingo";
+        $exercicio = "Exercicio concluido";
+    
+        echo $segunda_feira . "<br>";
+        echo $terca_feira . "<br>";
+        echo $quarta_feira . "<br>";
+        echo $quinta_feira . "<br>";
+        echo $sexta_feira . "<br>";
+        echo $sabado . "<br>";
+        echo $domingo . "<br>";
+        echo $exercicio;
+
+        
+
+    ?>
+</body>
+</html>

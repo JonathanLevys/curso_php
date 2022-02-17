9dc490eb6b8342a6a2cdee4ab08c63d48aba4c7d
Jonathan Levy <jonathan.adm2012@gmail.com>
2022-02-17 00:31:35 -0300

    exercitando formularios

--- aula06/exercicio_06_02/form_divisao.php
+++ aula06/exercicio_06_02/form_divisao.php
@@ -4,17 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MULTIPLICAÇÃO</title>
+    <title>DIVISÃO</title>
 </head>
 <body>
-    <form action="multiplicacao.php" method="post">
+    <form action="divisao.php" method="post">
         valor1: <input name= "valor1" type="text"><br><br>
         valor2: <input name= "valor2" type="text"><br><br>
-        <button type="submit">multiplicar</button><br><br>
+            <button type="submit">DIVIDIR</button><br><br>
         Resultado: <input name= "resultado" type="text" value="<?php if(isset($_GET["resultado"])) echo $_GET["resultado"] ?>">
-            <button type="reset">Limpar
-
-            </button>
+            <button type="reset">LIMPAR</button>
     </form>
 </body>
 </html>

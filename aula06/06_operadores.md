abec95d4e8e2d3c5daefb83333af55541b0d41d1
Wellington Junior <wellington.junior@madeiramadeira.com.br>
2022-02-23 21:27:17 -0300

    aula06.05

--- aula06/06_operadores.md
+++ aula06/06_operadores.md
@@ -6,4 +6,5 @@ Os operadores nos permitem modificar ou obter novos valores a partir de uma ou m
 + [06.02 Operacao Aritmética ](06.02_operacao_aritmética.md)
 + [06.03 Operadores de decremento e incremento ](06.03_operadores_incremento_decremento.md)
 + [06.04 Operadores de atribuição ](06.04_operadores_de_atribuição.md)
++ [06.05 Operadores lógicos ](06.05_operadores_lógicos.md)
 
---
+++ aula06/operadores_logicos.php
@@ -0,0 +1,47 @@
+<?php 
+
+// codições logicas com operador condicional (AND)
+$a = true; 
+$b = true;
+
+// if($a and $b) {
+//     echo "são verdadeiros";
+// } else {
+//     echo "são falsos";
+// }
+
+// codições logicas com operador condicional (&&)
+// $a = true; 
+// $b = true;
+
+// if($a && $b) {
+//     echo "são verdadeiros";
+// } else {
+//     echo "são falsos";
+// }
+
+// codições logicas com operador condicional (OR)
+// $a = "wellington@gmail.com";
+// $b = "1234";
+
+// if ($a == "wellington@gmail.com" or $b == "123") {
+//     echo "são verdadeiros";
+// } else {
+//     echo "são falsos";
+// }
+
+// codições logicas com operador condicional (||)
+// $a = "wellington@gmail.com";
+// $b = "1234";
+
+// if ($a == "wellington@gmail.com" || $b == "123") {
+//     echo "são verdadeiros";
+// } else {
+//     echo "são falsos";
+// }
+
+// if(!$a) {
+//     echo "são verdadeiros";
+// } else {
+//     echo "são falsos";
+// }

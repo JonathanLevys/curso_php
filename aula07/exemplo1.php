7cd914372e0388f45be6722bf0997248e8b16791
Wellington Junior <wellington.junior@madeiramadeira.com.br>
2022-02-24 21:11:56 -0300

    correçao aula07

---
+++ aula07/exemplo1.php
@@ -0,0 +1,14 @@
+<?php 
+// PSR-2
+
+$hora = 01;
+
+if ($hora <= 12) {
+  echo "Tenha uma bom dia!";
+} else if ($hora <= 18) {
+  echo "Tenha uma boa tarde!";
+} else {
+  echo "Tenha uma boa noite!";
+}
+
+?> 

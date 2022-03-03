57ddcf4543597b79ba58eb2b6ba816c0e31901f9
Jonathan Levy <jonathan.adm2012@gmail.com>
2022-03-02 21:30:45 -0300

    exercitando

---
+++ aula07/exercicicios/exerc_22/form_exerc22.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>formulario22</title>
+</head>
+<body>
+    <form action="exerc22.php" method="post">
+        LADO A: <input name="valor1" type="text">
+        LADO B: <input name="valor2" type="text">
+        LADO C: <input name="valor3" type="text">
+            <button type="submit">CALCULAR</button>
+    </form>
+</body>
+</html>

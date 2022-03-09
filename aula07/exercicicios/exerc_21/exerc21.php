be9698412b0f0d3220da8c21ea907a6ac53ee88f
Jonathan Levy <jonathan.adm2012@gmail.com>
2022-03-09 18:01:18 -0300

    exercitando

--- aula07/exercicicios/exerc_21/exerc21.php
+++ aula07/exercicicios/exerc_21/exerc21.php
@@ -7,15 +7,15 @@ $media = ($nota1 + $nota2) / 2;
 
 
 if ($media < 4) {
-    echo " E";
+    echo " E " . "REPROVADO";
 } elseif ($media > 4 and $media < 6) {
-    echo " D";
+    echo " D " . "REPROVADO";
 } elseif ($media > 6 and $media < 7.5) {
-    echo "C";
+    echo "C " . "APROVADO";
 } elseif ($media >7.5 and $media < 9) {
-    echo "B";
+    echo "B " . "APROVADO";
 } else {
-    echo "A";
+    echo " A " . "APROVADO";
 }
 
 ?>

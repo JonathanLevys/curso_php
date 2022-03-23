be1bc4dbf4d21272e2fdd26777355549f028c0d1
Jonathan Levy <jonathan.adm2012@gmail.com>
2022-03-22 22:24:21 -0300

    exercicios

---
+++ aula09/Exercicicios/exerc02.php
@@ -0,0 +1,13 @@
+<?php
+    $a = $_POST["valor"];
+
+    while ($a = 1000 . $a <= 2000 . $a++) {
+        if ($a % 11 == 2) {
+
+        }
+        
+        echo "Numeros: " . $a;
+    }
+
+
+?>
---
+++ aula09/Exercicicios/form_exerc02.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="exerc02.php" method="post">
+        Digite um numero entre 1000 e 2000.
+        Digite: <input name="valor" type="text">
+            <button type="submit">CALCULAR</button>
+    </form>
+</body>
+</html>
